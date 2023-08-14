db447a8b40918f5ce368e7fd743f6c71b93e67e7
Karthickeyen-M <91118100+Karthickeyen-M@users.noreply.github.com>
2023-08-14 14:28:44 +0530

    Update librarymanagement.java

--- librarymanagement.java
+++ librarymanagement.java
@@ -1,96 +1,174 @@
+package lib;
 import java.util.*;
-abstract class Admin {
-
-
+public class librarymanagement {
+	static ArrayList<String>books = new ArrayList<>();
+	static ArrayList<String>authors = new ArrayList<>();
+	static ArrayList<Integer>years = new ArrayList<>();
+	static ArrayList<Integer>ids = new ArrayList<>();
+	static ArrayList<String>titles = new ArrayList<>();
+	static HashMap<String,Integer>h1 = new HashMap<>();
+	static HashMap<String,Integer>h2 = new HashMap<>();
+		public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in);
+//	       System.out.println("Enter your role (Admin/User): ");
+//	       String role = sc.nextLine();
+//	       if(role.equals("Admin")){
+	       System.out.println("Enter the no.of entries: ");
+	       int n = sc.nextInt(),f=0;
+	       sc.nextLine();
+	       for(int i=0 ; i<n ;) {
+	    	   f=0;
+	    	   System.out.println("Enter your choice (Book/Magazine): ");
+	    	   String b = sc.nextLine();
+	    	   if(b.equals("Book"))
+	    	   {
+	    		   f=1;
+	    	   System.out.println("Enter the bookname: ");
+	    	   String bookname = sc.nextLine();
+	    	   System.out.println("Enter the Authorname: ");
+	    	   String authorname = sc.nextLine();
+	    	   System.out.println("Enter the year of publishing: ");
+	    	   int year = sc.nextInt();
+	    	   Books b1 = new Books();
+	    	   Author a1 = new Author();
+	    	   b1.setName(bookname);
+	    	   System.out.println(b1.getName());
+	    	   a1.setAuthor(authorname);
+	    	   System.out.println(a1.getAuthor());
+	    	   b1.setYear(year);
+	    	   System.out.println(b1.getYear());
+	    	   i++;
+	    	   }
+	    	   else if(b.equals("Magazine")){
+	    		   f=1;
+	    	   System.out.println("Enter the title of the magazine: ");
+	    	   String magazine = sc.nextLine();
+	    	   System.out.println("Enter the id of the Magazine: ");
+	    	   int id = sc.nextInt();
+	    	   Magazine m1 = new Magazine();
+	    	   m1.setMagazine(magazine);
+	    	   System.out.println(m1.getMagazine());
+	    	   m1.setId(id);
+	    	   System.out.println(m1.getId());
+	    	   i++;
+	    	   }
+	    	   if(f==0)System.out.println("Enter a valid entry...");
+	       }
+	       
+	       System.out.println("Book entries are as follows \n"+h1);
+	       System.out.println("Magazine entries are as follows \n"+h2);
+	       //}
+//	       else{
+//	    	   
+//	       }
+		}
 }
-	class Books extends Admin{
-	String booname = null;
-	int year ;
-	//Books(String)
+
+
+
+
+
+
+
+
+
+package lib;
+
+public class Books extends librarymanagement{
+	String bookname;
+	int year;
+	public Books() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setName(String bookname) {
+		this.bookname = bookname;
+		books.add(bookname);
+		if(!h1.containsKey(bookname)){
+			h1.put(bookname, 1);
+		}
+		else{
+			int v = h1.get(bookname);
+			h1.put(bookname, ++v);
+		}
+	}
+
 	public String getName() {
-		return booname;
+		return bookname;
 	}
-	public void setName(String name) {
-		this.booname = name;
+
+	public void setYear(int year) {
+		this.year = year;
+		years.add(year);
 	}
+
 	public int getYear() {
 		return year;
 	}
-	public void setYear(int year) {
-		this.year = year;
-	}
+
 }
-class Author extends Books{
-	String author = null;
+
+
+
+
+
+package lib;
+
+public class Author extends librarymanagement{
+	String authorname;
+	public Author() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setAuthor(String authorname) {
+		this.authorname = authorname;
+		authors.add(authorname);
+	}
 
 	public String getAuthor() {
-		return author;
+		return authorname;
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
 }
-class Magazine {
-	String magazine =null;
+
+
+
+
+
+package lib;
+
+public class Magazine extends librarymanagement{
+	String magazine;
 	int id;
+	public Magazine() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setMagazine(String magazine) {
+		this.magazine = magazine;
+		titles.add(magazine);
+		if(!h2.containsKey(magazine)){
+			h1.put(magazine, 1);
+		}
+		else{
+			int v = h1.get(magazine);
+			h1.put(magazine, ++v);
+		}
+	}
+	public String getMagazine() {
+		return magazine;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+		ids.add(id);
+	}
 
 	public int getId() {
 		return id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getMagazine() {
-		return magazine;
-	}
-
-	public void setMagazine(String magazine) {
-		this.magazine = magazine;
-	}
-	void display(String aname , String bname) {
-		System.out.println("Book :"+aname+","+"Author:"+bname);
-	}
 }
-public class librarymanagement{
-	public static void main(String[] args) {
-	
-       Scanner sc = new Scanner(System.in);
-       System.out.println("Enter the no.of books");
-       int n = sc.nextInt();
-       sc.nextLine();
-       String b = sc.nextLine();
-       for(int i =0 ; i<n ;i++) {
-    	   
-    	   if(b.equals("Book"))
-    	   {
-       //String b = sc.nextLine();
-    	   String bookname = sc.nextLine();
-    	   String authorname = sc.nextLine();
-    	   int year = sc.nextInt();
-    	   Books b1 = new Books();
-    	   Author a1 = new Author();
-    	   b1.setName(bookname);
-    	   System.out.println(b1.getName());
-    	   a1.setAuthor(authorname);
-    	   System.out.println(a1.getAuthor());
-    	   b1.setYear(year);
-    	   System.out.println(b1.getYear());
-       }else {
-    	   String magazine = sc.nextLine();
-    	   int id = sc.nextInt();
-    	   Magazine m1 = new Magazine();
-    	   m1.setMagazine(magazine);
-    	   System.out.println(m1.getMagazine());
-    	   m1.setId(id);
-    	   System.out.println(m1.getId());
-       }
-       }
-       sc.close();
-       
-       
-	}
 
-}
+
+
